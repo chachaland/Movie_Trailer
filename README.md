c4bc10bea4f8dd524fa1c5f85df87edba5f1051e
Jeesoo Lee <ijsoo99@hotmail.com>
2018-01-30 04:14:37 -0500

    Changes made for resubmit
    Changes:
    1) Code formatting --> checked with PEP8 online
    2) One of the youtube link not opening replaced
    3) Better documentation for Class Movie
    4) README.md changed

--- README.md
+++ README.md
@@ -8,6 +8,7 @@ This project generates a website of movie trailers
 
 ## How to Run:
 
-1. Clone this repository then run `fresh_tomatoes.py`. 
-2. Movie Trailer Website `fresh_tomatoes.html` will be generated
-3. Can view the page by opening `fresh_tomatoes.html` 
+1. Clone this repository 
+2. Open Python IDLE, select File --> Open, open `entertainment_center.py`
+3. Select Run --> Run Module, Movie Trailer Website `fresh_tomatoes.html` will be generated and opened on the browser
+4. Also, can view the page by opening `fresh_tomatoes.html` 
--- media.py
+++ media.py
@@ -1,17 +1,32 @@
 import webbrowser
 
-class Movie(object):
-	"""This class provided a way to store movie related information"""	
-	VALID_RATINGS = ["G","PG","PG-13","R"]
 
-	#storing movie related information 	 
-	def __init__(self, movie_title, movie_storyline, poster_image,trailer_youtube):
-		self.title = movie_title
-		self.storyline = movie_storyline
-		self.poster_image_url = poster_image
-		self.trailer_youtube_url = trailer_youtube
-	
-	#opening a trailer from the web	
-	def show_trailer(self):
-		webbrowser.open(self.trailer_youtube_url)	
-		
+class Movie(object):
+    """Class providing a way to store movie related information
+
+        Args:
+                movie_title (str): Title of a movie
+                movie_storyline (str): Storyline of a movie
+                poster_image (str): url of a poster image of a movie
+                trailer_youtube_url (str): url of a movie trailer
+
+        Attributes:
+                movie_title (str): Title of a movie
+                movie_storyline (str): Storyline of a movie
+                poster_image (str): url of a poster image of a movie
+                trailer_youtube_url (str): url of a movie trailer
+
+        Methods:
+                show_trailer: opening a trailer from the web
+    """
+
+    def __init__(self, movie_title, movie_storyline, poster_image,
+                 trailer_youtube):
+        self.title = movie_title
+        self.storyline = movie_storyline
+        self.poster_image_url = poster_image
+        self.trailer_youtube_url = trailer_youtube
+
+    def show_trailer(self):
+        """Open a trailer from the web"""
+        webbrowser.open(self.trailer_youtube_url)
